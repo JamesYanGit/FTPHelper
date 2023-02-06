8c2cd80c6a7f5d22f6408b3a0ce493427d0b63bc
James <yyan4968@upei.ca>
2023-02-06 19:50:18 -0400

    Add project files.

---
+++ FTPHelperClass.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Security.Policy;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FTPHelper
+{
+    public class FTPHelperClass
+    {
+        private string _hostServer;
+        private string _hostName;
+        private string _hostPsw;
+        private string _hostDirectory;
+        public FTPHelperClass(string hostServer, string hostName, string hostPsw, string directory) 
+        {
+            _hostServer=hostServer;
+            _hostName=hostName;
+            _hostPsw=hostPsw;
+            _hostDirectory=directory;
+        }
+        public bool onload(string file)
+        {
+            //构造一个web服务器的请求对象
+            FtpWebRequest ftp;
+            //实例化一个文件对象
+            FileInfo f = new FileInfo(file);
+            ftp = (FtpWebRequest)FtpWebRequest.Create(new Uri(@"ftp://"+_hostServer + _hostDirectory + f.Name));
+            //创建用户名和密码
+            ftp.Credentials = new NetworkCredential(_hostName, _hostPsw);
+            ftp.KeepAlive = false;
+            ftp.Method = WebRequestMethods.Ftp.UploadFile;
+            ftp.UseBinary = true;
+            ftp.ContentLength = f.Length;
+            int buffLength = 20480;
+            byte[] buff = new byte[buffLength];
+            int contentLen;
+            try
+            {
+                //获得请求对象的输入流
+                FileStream fs = f.OpenRead();
+                Stream sw = ftp.GetRequestStream();
+                contentLen = fs.Read(buff, 0, buffLength);
+                while (contentLen != 0)
+                {
+                    sw.Write(buff, 0, contentLen);
+                    contentLen = fs.Read(buff, 0, buffLength);
+                }
+                sw.Close();
+                fs.Close();
+                return true;
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+                return false;
+            }
+        }
+
+        public string fload(string fileName)
+        {
+            try
+            {
+                FtpWebRequest ftp;
+                ftp = (FtpWebRequest)FtpWebRequest.Create(new Uri(@"ftp://" + _hostServer + _hostDirectory + fileName));
+                //指定用户名和密码
+                ftp.Credentials = new NetworkCredential(_hostName, _hostPsw);
+                WebResponse wr = ftp.GetResponse();
+                StreamReader sr = new StreamReader(wr.GetResponseStream(), System.Text.Encoding.Default);
+                string s = sr.ReadLine();
+                while (s.Equals(""))
+                {
+                    s = sr.ReadLine();
+                }
+                return s;
+            }
+            catch (Exception)
+            {
+                //return "serverDown";
+                //throw;
+            }
+            return "";
+            
+        }
+
+        public string detectFTPServer() 
+        {
+            FtpWebRequest ftp;
+            ftp = (FtpWebRequest)FtpWebRequest.Create(new Uri(@"ftp://" + _hostServer + _hostDirectory));
+            //指定用户名和密码
+            ftp.Credentials = new NetworkCredential(_hostName, _hostPsw);
+            ftp.Method=WebRequestMethods.Ftp.PrintWorkingDirectory;
+            ftp.KeepAlive = false;
+            try
+            {
+                WebResponse wr = ftp.GetResponse();
+                return "";
+            }
+            catch 
+            {
+
+                return "serverDown";
+            }
+            
+        }
+
+
+        public string DeleteFile(string fileName)
+        {
+            FtpWebRequest request = (FtpWebRequest)WebRequest.Create(@"ftp://" + _hostServer + _hostDirectory + fileName);
+            request.Method = WebRequestMethods.Ftp.DeleteFile;
+            request.Credentials = new NetworkCredential(_hostName, _hostPsw);
+
+            using (FtpWebResponse response = (FtpWebResponse)request.GetResponse())
+            {
+                return response.StatusDescription;
+            }
+        }
+    }
+}
---
+++ Interface/IFTPHelper.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FTPHelper.Interface
+{
+    public interface IFTPHelper
+    {
+        /// <summary>
+        /// Create a direcotry
+        /// </summary>
+        /// <param name="directoryPath"></param>
+        void CreateDirectory(string directoryPath);
+
+        /// <summary>
+        /// Delete a directory
+        /// </summary>
+        /// <param name="directoryPath"></param>
+        void DeleteDirectory(string directoryPath);
+
+        /// <summary>
+        /// Upload a file to FTP server
+        /// </summary>
+        /// <param name="localFullPath"></param>
+        void UploadFile(string localFullPath);
+
+        /// <summary>
+        /// if overwrite the exsiting file.
+        /// </summary>
+        /// <param name="localFullPath"></param>
+        /// <param name="overWriteFile"></param>
+        void UploadFile(string localFullPath, bool overWriteFile);
+
+        /// <summary>
+        /// Upload files as byte array
+        /// </summary>
+        /// <param name="fileBytes"></param>
+        /// <param name="remoteFileName"></param>
+        void UploadFile(byte[] fileBytes, string remoteFileName);
+    }
+}
